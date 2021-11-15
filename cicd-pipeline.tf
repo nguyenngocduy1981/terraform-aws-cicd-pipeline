75fa8d4cf790cb81ba52c7d33d3d6c6cca5f7be7
DuyNN <nguyenngocduy1981@gmail.com.com>
2021-11-15 22:27:20 +0700

    update git repo

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -69,7 +69,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
       version          = "1"
       output_artifacts = ["tf-code"]
       configuration = {
-        FullRepositoryId     = "davoclock/aws-cicd-pipeline"
+        FullRepositoryId     = "nguyenngocduy1981/terraform-aws-cicd-pipeline"
         BranchName           = "master"
         ConnectionArn        = var.codestar_connector_credentials
         OutputArtifactFormat = "CODE_ZIP"
