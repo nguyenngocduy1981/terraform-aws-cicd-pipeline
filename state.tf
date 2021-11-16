d3785f340dc9eb5b2b3f393f992c6f630b262d67
DuyNN <nguyenngocduy1981@gmail.com.com>
2021-11-16 10:28:38 +0700

    update

--- state.tf
+++ state.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-aws-cicd-pipeline"
-    encrypt = true
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-  }
+terraform{
+    backend "s3" {
+        bucket = "terraform-aws-cicd-pipeline"
+        encrypt = true
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
 }
 
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-dockerhub_credentials          = "arn:aws:secretsmanager:us-east-1:776739115685:secret:codebuild/dockerhub-SuKt0i"
+dockerhub_credentials = "arn:aws:secretsmanager:us-east-1:776739115685:secret:codebuild/dockerhub-SuKt0i"
 codestar_connector_credentials = "arn:aws:codestar-connections:us-east-1:776739115685:connection/51fd2a83-b751-4983-9d10-f81fcd169a6a"
